0501790c5db9ba006055dfb81f806e77ab4c0849
jd <root@example.com>
2025-02-27 15:12:42 +0100

    fix

--- frontend-src/App.jsx
+++ frontend-src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = window.RUNTIME_CONFIG?.API_URL || import.meta.env.VITE_API_URL;
+console.log("API URL:", API_URL);
 
 
 function App() {
---
+++ frontend-src/runtime-config.js
@@ -0,0 +1,4 @@
+window.RUNTIME_CONFIG = {
+	VITE_API_URL: "http://backend:3000"
+};
+
